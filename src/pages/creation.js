1be51e078c22d66049891dec73c60bd463f4afad
Muahahayes <muahahayes@gmail.com>
2020-04-09 21:49:02 -0600

    added creation page

--- src/pages/creation.js
+++ src/pages/creation.js
@@ -3,6 +3,29 @@ import { Link } from 'react-router-dom';
 export default {
   name:"Character Creation",
   content: <div>
-    
+    <span>In Breach the characters Players create start off mechanically simple compared to other Tabletop RPGs. The customization and variety of character choices in Breach come from the leveling process, so to create a character from scratch at level 1 is very straightforward (to create a character of higher level, read further below).<br/></span>
+    <h3>Creating a Character follows this 8 step process:</h3>
+    <ul>
+      <li><b>Write your 5 <Link to="/rules/quirks">Quirks</Link>:</b> It's best to have an idea of who your character is before you make mechanical choices to bring them into the game. If you end up with a character that doesn't fully fit your Quirks, you can come back and change these before you finish.</li>
+      <li><b>Select your <Link to="/races">Race</Link> (and possibly a Subrace):</b> Choose a Race to represent your character, your character's appearance is cusomizable within the bounds of their race. Your character gains all of the racial features of that Race, and base stats.</li>
+      <li><b>Assign your <Link to="/rules/stats">Attribute</Link> Points:</b> Using the Creation AP from your Race, distribute these points across your 4 Attributes.</li>
+      <li><b>Calculate your <Link to="/rules/stats">Life and Survival</Link>:</b> Your character starts with a maximum of 5 Life. Increase or decrease your Life by an amount equal to your Stamina (to a minimum of 1 Life). Your character starts with a maximum of 10 Survival. Increase or decrease your Survival by an amount equal to your Stamina (to a minimum of 5 Survival).</li>
+      <li><b>Assign your <Link to="/rules/skills">Skill</Link> Points:</b> Using the Creation SP from your Race, distribute these points across your Skills. A Skill may never be more than 5 times your level. If a Skill is not listed in your Race's Favored Skills, it costs 2 points for every Skill point increased. Any Skills you did not assign points to will start at 0.</li>
+      <li><b>Select your <Link to="/sources">Source</Link>:</b> This Source will be the origin of your character's Gifts, it sets them apart from the common public.</li>
+      <li><b>Select your first <Link to="/gifts">Gift</Link>:</b> You may only select Gifts from your Source, and it must be [1 G]. As you level up you will select a new Gift at each level, and it must be a [G] equal or less than your level.</li>
+      <li><b>Starting Equipment:</b> Coming Soon!</li>
+    </ul>
+    <hr/>
+    <h3>Creating a Character at higher levels:</h3>
+    <span>Consult the chart at the bottom of the <Link to="/rules/leveling">XP and Leveling Up</Link> page. Increase your maximum Survival by your Stamina times your Level and for every even numbered level you <i>skipped</i> increase one Attribute by 1. For each level you <i>skipped</i> add together the Proficiencies. Distribute this value across your Skills, keep in mind a Skill may never be more than 5 times your level. Then, for each level you <i>skipped</i> select a new Gift with a [G] equal or less than that level, and distribute that level's Proficiencies across your current Gifts and Source. Finally, select a new Gift with a [G] equal or less than your current level, and if your level is even numbered increase one Attribute by 1.</span>
+    <hr/>
+    <h3>Example Character Build:</h3>
+    <span>Bob wants to play Breach with his friends, but he needs a character first! Bob's always been a fan of sneaky lizard races in his RPG playing, so he names his character Argo and writes 5 Quirks that describe him as a thief who upset a crime syndicate and is now on the run.
+      He selects the Atal race, specifically the Akkso'Atal subrace and he writes down the Atal racial features as well as the Akkso features. He has 3 AP to distribute, he puts 2 into Agility and 1 into Stamina so he is sneaky but has some more survivability. He has 2 Stamina, so he has 5 + 2 maximum Life and 10 + 2 maximum Survival.
+      Next, Bob looks at the 8 Skill Points given by the Atal race. He puts 5 into Sneak (his limit is 5 at level 1, since Skills cannot go above 5 times your level) and the remaining 3 into Hide. Now his character Argo is sneaky and can be a more effective "Thief", the arbitrary Role Quirk he had written.
+      Now that Bob's character has an identity, race, and skills, he looks at the Sources to find one that best fits the playstyle he's going for. He likes the Dermal Markings Gift because of its Hide boosting Proficiency called Camouflage, along with some other melee focused abilities, in the Mutation Source so he selects Mutation. 
+      He looks through Mutation's [1 G] Gifts and settles on Retractable Claws as his first choice, it helps him in combat right away and he can pick up Dermal Markings at level 2 when he wants more utility.
+      Finally, Bob looks through the Equipment Table and (equipment rules here, he selects some starter gear with some kind of budget). Bob fills out his character sheet with these choices and can now take Argo to the table to play with his friends next weekend, creation is complete!
+    </span>
   </div>
 }
--- src/rules/quirks.js
+++ src/rules/quirks.js
@@ -5,7 +5,7 @@ export default {
   content: <div>
     Select one single sentence or phrase for each of the 5 Quirks below. Try to select 5 Quirks which do not paint the same picture at first glance, more variety leads to a more fleshed out character and more situations where a Quirk can come into play! <br/>
     If a Quirk would probably apply to ¼ or more combat encounters during the campaign, reword or replace the Quirk (ie. Role: Master Black Belt, the player may ask for a bonus to any melee fight they encounter, try something like Wise Monk and emphasize the philosophy of martial arts more than just the combat ability). <br/>
-    Whenever a Quirk would reasonably give you more experience or aptitude in a situation, apply a Favored +1 to your rolls. If a Quirk would reasonably increase the difficulty of a situation, apply an Unfavored -1 to your rolls but you're awarded 3 xp instead of 1 for any Hard Success that would award xp and 1 xp for any other result. You cannot apply more than 1 Quirk to a single roll.<br/>
+    Whenever a Quirk would reasonably give you more experience or aptitude in a situation, apply a Favored +1 to your rolls. If a Quirk would reasonably increase the difficulty of a situation apply an Unfavored -1 to your rolls, but you're awarded 3 <Link to="rules/leveling">XP</Link> instead of 1 for any Hard Success that would award XP, and 1 XP for any other result. You cannot apply more than 1 Quirk to a single roll.<br/>
 		<hr/>
     <h3>The 5 Character Quirks:</h3>
     <ul>
