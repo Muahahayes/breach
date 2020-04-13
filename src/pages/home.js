c56277d185368d1c261d160a083f0faa9754b2d9
Muahahayes <muahahayes@gmail.com>
2020-04-13 02:42:58 -0600

    rewrote some content

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,10 +3,13 @@ import {Link} from 'react-router-dom';
 export default {
   "name": "BREACH",
   "content": [
-   <p>Breach is a Tabletop RPG where friends can come together to go on adventures. Through your supernatural Gifts, you can use your newfound powers to fight monsters, wheel and deal your way to fortune, or enter the mysterious Breach to face the horrors of the universe!
-
-    If you've never played a Tabletop RPG (such as D&amp;D or Pathfinder), visit the <Link to="rpg">What's an RPG</Link> link on the left to learn the basics.
-
-    If this isn't your first rodeo, go ahead and check out the <Link to="new">What's New</Link> link instead for a quick rundown of what you need to know to get up to speed with Breach quickly.</p>
+   <p>
+      <h3>Welcome to the Breach Player's Guidebook!</h3>
+      Breach is a Tabletop RPG where friends can come together to go on adventures. Through your supernatural Gifts, you can use your newfound powers to fight monsters, wheel and deal your way to fortune, or enter the mysterious Breach to face the horrors of the universe!
+      If you've never played a Tabletop RPG (such as D&amp;D or Pathfinder), visit the <Link to="rpg">What's an RPG</Link> link on the left to learn the basics.
+      If this isn't your first rodeo, go ahead and check out the <Link to="new">What's New</Link> link instead for a quick rundown of what you need to know to get up to speed with Breach quickly.
+      <br/><br/>
+      After you've gotten your bearings and are ready to dive into the book, the bar on the left shows links to various pages detailing the rules of the game and the various Race, Sources, and Gifts you can select to customize your Character. At the top are links to the World and GM guides if you're up to the challenge of running your own campaign!
+    </p>
   ]
 }
