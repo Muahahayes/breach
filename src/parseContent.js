6bf5048b11a0882fd6b2013e8e37ab8acb744a8f
Muahahayes <muahahayes@gmail.com>
2020-04-16 00:58:56 -0600

    if it sees a react object it just returns it back

--- src/parseContent.js
+++ src/parseContent.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 
-//TODO: refactor this as a dev tool for turning strings into React elements to save time
-// have it output as a string, not as a react element though, so I can easily copy it into a .js file
 function parseContent(row, elements) {
   if (Array.isArray(row)) { // its an array, treat as a ul
     let el = [];
@@ -13,19 +11,22 @@ function parseContent(row, elements) {
     }
     elements.push(React.createElement('ul', {}, el));
   }
-  else { // its a string, parse for \n and : bolding
+  else if (typeof row === 'string') { // its a string, parse for \n and : bolding
     let lines = row.split('\n');
     for (let line of lines) {
       if (line !== '') {
         let str = line.split(':');
         let remaining = str.slice(1).join(':')
         str[0] = (str[0] !== '\\')?`${str[0]}:`:'';
-        // eslint-disable-next-line
+//eslint-disable-next-line
         let el = (str.length > 1)?<span><b>{`${str[0]}`}</b>{remaining}</span>:<span>{line}</span>;
         elements.push(el);
       }          
       elements.push(<br/>);
     }        
+  }
+  else {
+    elements.push(row);
   }
 }
 
