18010e471d42f37400820ee427780afe9ff324ba
Muahahayes <muahahayes@gmail.com>
2020-04-19 18:16:25 -0600

    reworded thick skin

--- src/sources/bloodline.js
+++ src/sources/bloodline.js
@@ -5,7 +5,7 @@ export default {
   resource: "Bloodlust: Whenever you deal damage to, or receive damage from a creature you gain 1 Bloodlust (once per creature per turn, max of [G * 2] number of creatures per turn). These Bloodlust points may be spent by various Powers within this Source. Whenever you fail to land an attack on an opponent you lose a Bloodlust. if you reach 0 Bloodlust you enter Apathy and are unable to use any Powers until you gain at least 1 Bloodlust to end your Apathy. You lose all of your Bloodlust when you have a Full Sleep, and half (rounded down) when you take a Break.",
   proficiencies: [
     [
-      "Thick Skin: Increase your max survival by 5. (Repeatable up to G)",
+      "Thick Skin: Increase your max survival by 5. (Repeatable up to G times)",
       "Retractable Canines: Your canine teeth are sharp and can extend to twice their length.",
       "Study: Whenever you level up, you may select 1 Lesser Gift for free without spending a Proficiency."
     ],
--- src/sources/mutation.js
+++ src/sources/mutation.js
@@ -5,7 +5,7 @@ export default {
   resource: "Metabolism: When you use an active Mutation you experience Strain. You lower your max survival for each point of Strain you gain, and regain this lowered max survival for each point of Strain you lose up to your unstrained maximum. You remove all of your Strain when you fully sleep or if you take a break while eating a full meal. You remove an amount of Strain equal to [Stamina * G] when you take a break without eating a full meal. If your Strain exceeds half your unstrained max survival you become Overstrained and become unconscious, you cannot use any Powers until you fully sleep or take a break while eating a full meal.",
   proficiencies: [
     [
-      "Thick Skin: Increase your max survival by 5. (Repeatable up to G)",
+      "Thick Skin: Increase your max survival by 5. (Repeatable up to G times)",
       "Retractable Canines: Your canine teeth are sharp and can extend to twice their length.",
       "Tail: If you did not already have one, gain a tail of your animal choice. You gain +1 to Agility checks."
     ],
