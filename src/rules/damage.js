a6476d8fd9c5f3eb77b0a9660f2907fdc2ff04b1
muahahayes <muahahayes@gmail.com>
2020-07-18 03:04:32 -0600

    stat tweaks

--- src/rules/damage.js
+++ src/rules/damage.js
@@ -3,38 +3,35 @@ import { Link } from 'react-router-dom';
 export default {
 name: 'Damage',
 content: <div>
-  <p><b>Damaging:</b> When dealing damage to a creature subtract the damage value from creature's Shielding, and then subtract the remaining damage, if any, from the creature's Survival. If the victim's Survival reaches 0, subtract 1 from the victim's Life and roll on the Injury Table below. Then, reset the victim's Survival to its maximum value and apply the remaining damage, if any, to their Survival. If this brings them to 0 Survival again repeat the Injury process but roll the severity die twice and keep the higher result for this and any further Injuries.<br/>
-  <b>Killing/Subduing:</b> When dealing damage the attacker may attempt to subdue and not kill the victim, if they reduce the victim's Life below 1 but not to a negative number higher than half the victim's max Life the victim is made unconscious, otherwise the victim is Dead. If the creature's Life drops below 1 from damage, the attacker intends to kill, and the creature is a Player, or for NPCs at your GM's discretion, apply the <Link to="/rules/stats">Death</Link> rules. Otherwise, the creature is immediately Dead.</p>
+  <p><b>Damaging:</b> When dealing damage to a creature subtract the damage value from creature's Shielding, and then subtract the remaining damage, if any, from the creature's Survival. If the victim's Survival reaches 0, subtract 1 from the victim's Life and roll on the Injury Table below. Then, reset the victim's Survival to its maximum value and apply the remaining damage, if any, to their Survival. If this brings them to 0 Survival again repeat the Injury process but roll the severity die twice and keep the higher result.<br/><br/>
+  <b>Killing/Subduing:</b> When dealing damage the attacker may attempt to subdue and not kill the victim, if they reduce the victim's Life below 1 but not to a negative number higher than the victim's max Life the victim is made unconscious, otherwise the victim is Dead. If the creature's Life drops below 1 from damage, the attacker intends to kill, and the creature is a Player, or for NPCs at your GM's discretion, apply the <Link to="/rules/stats">Death</Link> rules. Otherwise, the creature is immediately Dead.</p>
   <hr/>
   <h3>Types of Damage:</h3>
-  <p><b>Resistance:</b> When you are Resistant to a type of Damage, subtract G from all damage from that type. Additionally, whenever that type would bring you to 0 Survival, roll a d20 vs the remaining damage. On a success, cancel the remaining damage and set your Survival to 1.<br/><br/>
+  <p><b>Resistance:</b> When you are Resistant to a type of Damage, subtract G from all damage received from that type. Additionally, whenever that type of damage would bring you to 0 Survival, roll a d20 vs the remaining damage. On a success, cancel the remaining damage and set your Survival to 1.<br/><br/>
   <b>Physical types:</b><br/>
   <ul>
-    <li><b>Blunt:</b> On a critical hit, Stuns biological type creatures for 1 turn.<br/></li>
-    <li><b>Sharp:</b> On a critical hit, Bleeds biological type creatures every 5 turns and causes G Strain.<br/></li>
-    <li><b>Crushing:</b> On a critical hit, deals an additional Gd4 damage and causes G Faults.<br/><br/></li>
+    <li><b>Blunt:</b> Damage caused by impact with a flat non-sharp object.</li>
+    <li><b>Sharp:</b> Damage caused by being cut, stabbed, or sliced.</li>
+    <li><b>Crushing:</b> Damaged caused by being crushed, squeezed, or pressured.</li><br/>
   </ul>
   <b>Elemental types:</b><br/>
   <ul>
-    <li><b>Fire:</b> On a critical hit, Burns the target causing G fire damage over G turns.<br/></li>
-    <li><b>Water:</b> On a critical hit, Freezes the target snaring them for G turns.<br/></li>
-    <li><b>Nature:</b> On a critical hit, Poisons the target dealing G Nature damage over 4 turns with difficulty [15 + G], and subtracting G Bloodlust.<br/></li>
-    <li><b>Electric:</b> On a critical hit, Paralyzes the target for G turns. Cybernetic type creatures are also Stunned for the first turn.<br/><br/></li>
+    <li><b>Fire:</b> Damaged caused by extreme heat or burning.</li>
+    <li><b>Water/Ice:</b> Damaged caused by drowning, extreme cold, or crashing waves.</li>
+    <li><b>Nature:</b> Damaged caused by poisons, disease, or supernatural plant life.</li>
+    <li><b>Electric:</b> Damaged caused by lightning, electricity, or other electromagnetic energy.</li><br/>
   </ul>
   <b>Mental types:</b><br/>
   <ul>
-    <li><b>Psychic:</b> On a critical hit, Disorients the target subtracting G from their next attack roll.<br/></li>
-    <li><b>Kinetic</b> (deals Blunt damage if it hits): On a critical hit, Stuns the target for 1 turn.<br/></li>
-    <li><b>Spirit:</b> On a critical hit, Drains the target, damaging them and heals the attacker for Gd4 Life.<br/></li>
-    <li><b>Darkness:</b> On a critical hit, Blinds the target for 1 turn and subtracts G ae.</li>
+    <li><b>Psychic:</b> Damaged caused by assaults on the mind.</li>
+    <li><b>Kinetic:</b> Damaged caused to the body by the powers of the mind, deals Blunt type damage when it successfully hits.</li>
+    <li><b>Spirit:</b> Damage caused by the raw Aether within living things.</li>
+    <li><b>Darkness:</b> Damage which pierces the Aether within a sentient creature's soul.</li>
   </ul>  
   </p>
   <hr/>
   <h3 id="injury">Injury:</h3>
-  <p>When you receive an injury for any reason roll 2d10, the first roll decides the severity.<br/>
-  The second roll decides where the injury occurs (Physical/Elemental damage effects the body, and Mental effects the mind).<br/>
-  If at any time all 5 areas of the Body or Mind have an Extreme injury you instantly drop to 0 Life and become <Link to="/rules/stats">Dying</Link>.<br/>
-  Life points for Heavy or higher severity Injuries cannot be healed until the Injury is cured. Injuries which are not a Disability can be cured by a Full Sleep or some Powers/Techniques.</p>
+  <p>When you receive an injury for any reason roll 2d10, the first roll decides the severity. The second roll decides where the injury occurs (Physical/Elemental damage effects the body, and Mental effects the mind). If at any time all 5 areas of the Body or Mind have an Extreme injury you instantly drop to 0 Life and become <Link to="/rules/stats">Dying</Link>. When a Life point is healed you may select 1 Injury to be removed. Life points for Extreme severity Injuries cannot be healed until the Injury is cured.</p>
   <h3>Simplified Injury Table:</h3>
   <p>While playing the Simplified ruleset, or for non-person creatures, roll 1d10 on the Simplified Injury Table and do not worry about the location of the Injury.</p>
   <table>
@@ -47,30 +44,28 @@ content: <div>
     </tbody>
   </table>
   <h3>Full Injury Tables:</h3>
-  <p>While playing the Full Ruleset, roll 2d10. Apply the first d10 to the Severity table. Apply the second d10 to the Body or Mind tables for Physical/Elemental or Mental damage types respectively. <br/>
-    If the attack that caused the Injury was a Critical Hit and you roll a 6 or higher on the Severity Table, treat this as a Disabling Injury. The Injury cannot be cured by a Full Sleep or Powers/Techniques unless it specifically heals Disability.</p>
+  <p>While playing the Full Ruleset, roll 2d10. Apply the first d10 to the Severity table. Apply the second d10 to the Body or Mind tables for Physical/Elemental or Mental damage types respectively.</p>
   <div className="tableDiv">
     <table>
       <caption>Severity Table</caption>
       <tbody> 
       <tr><td>d10</td><td>Severity</td><td>Effect</td></tr>
-      <tr><td>1-2</td><td>Mild</td><td>No Effect</td></tr>
-      <tr><td>3-5</td><td>Moderate</td><td>Read Body/Mind</td></tr>
-      <tr><td>6-8</td><td>Heavy</td><td>Read Body/Mind</td></tr>
-      <tr><td>9-10</td><td>Extreme</td><td>Read Body/Mind</td></tr>
+      <tr><td>1-3</td><td>Mild</td><td>No Effect</td></tr>
+      <tr><td>4-6</td><td>Moderate</td><td>Read Body/Mind</td></tr>
+      <tr><td>7-9</td><td>Heavy</td><td>Read Body/Mind</td></tr>
+      <tr><td>10</td><td>Extreme</td><td>Read Body/Mind</td></tr>
       </tbody>
     </table>
   </div>
-  <div className="tableDiv">
-    <div className="injuryDiv">
+    <div className="tableDiv">
       <table className="injuryTable">
         <caption>Body Injuries</caption>
         <tbody> 
         <tr><td>d10</td><td>Severity</td><td>Moderate</td><td>Heavy</td><td>Extreme</td></tr>
-        <tr><td>1-2</td><td>Legs</td><td>-5ft speed</td><td>Moderate and always Prone</td><td>Heavy and can't use Movement Action</td></tr>
+        <tr><td>1-2</td><td>Legs</td><td>-1 speed</td><td>-G speed and knocked Prone</td><td>Heavy and can't use Movement Action</td></tr>
         <tr><td>3-4</td><td>Lower Body</td><td>-1 Avoidance</td><td>-G Avoidance</td><td>Heavy and 1/2 Carry Weight</td></tr>
         <tr><td>5-6</td><td>Arms</td><td>-1 Attack Rolls</td><td>-G Attack Rolls</td><td>Heavy and cannot use 1 arm</td></tr>
-        <tr><td>7-9</td><td>Upper Body</td><td>-G Survival</td><td>Moderate and lose 1 additional Life</td><td>Heavy but -1/2 Survival instead of -G</td></tr>
+        <tr><td>7-9</td><td>Upper Body</td><td>-G Max Survival</td><td>Moderate and lose 1 additional Life</td><td>Heavy but -1/2 Survival instead of -G</td></tr>
         <tr><td>10</td><td>Head</td><td>Unfavored -1 All Rolls (up to -3)</td><td>Moderate and roll a Moderate effect from the Mental Damage injury table</td><td>Heavy and Unconscious until this Injury is cured</td></tr>
         </tbody>
       </table>
@@ -82,12 +77,11 @@ content: <div>
         <tr><td>d10</td><td>Severity</td><td>Moderate</td><td>Heavy</td><td>Extreme</td></tr>
         <tr><td>1-2</td><td>Memory</td><td>On your next turn you cannot use an action on any creature which is not in line of sight at the start of your turn</td><td>Give your character sheet to the GM, you cannot look at the sheet until this injury is cured</td><td>Heavy and any time you try to recall a name, location, or event roll a d4, on a 1 you don't remember any of it and on a 2 or 3 you forget some meaningful detail</td></tr>
         <tr><td>3-4</td><td>Balance</td><td>-1 Avoidance</td><td>-G Avoidance</td><td>Heavy and 1/2 Carry Weight</td></tr>
-        <tr><td>5-6</td><td>Sensory</td><td>-1 Attack Rolls</td><td>-G Attack Rolls</td><td>Heavy and cannot use 1 arm</td></tr>
+        <tr><td>5-6</td><td>Sensory</td><td>-1 Attack Rolls</td><td>-G Attack Rolls</td><td>Heavy and lose sense of touch</td></tr>
         <tr><td>7-9</td><td>Emotional</td><td>-G Max Survival</td><td>Moderate and lose 1 additional Life</td><td>Heavy but -1/2 Survival instead of -G</td></tr>
         <tr><td>10</td><td>Cognitive</td><td>Unfavored -1 All Rolls (up to -3)</td><td>Moderate and roll a Moderate effect from the Mental Damage injury table</td><td>Heavy and Asleep until this Injury is cured</td></tr>
         </tbody>
       </table>
     </div>
-  </div>
 </div>
 }
--- src/rules/stats.js
+++ src/rules/stats.js
@@ -5,15 +5,15 @@ export default {
   content: 
   <div>
     <h3><b>Health:</b></h3>
-    <span><b>Life:</b> Your character has a maximum of 5 Life, plus additional Life equal to your Stamina (a minimum of 1 Life if your Stamina is -4 or less). Whenever you lose a point of Life, you receive an Injury. Roll on the Injury table at the bottom of this page. </span><br/>
+    <span><b>Life:</b> Your character has a maximum of 3 Life, plus or minus Life equal to half your Stamina (a minimum of 1 Life if your Stamina is -4 or less). Whenever you lose a point of Life, you receive an Injury. Roll on the Injury Table at the bottom of the <Link to="damage">Damage</Link> page. </span><br/>
     <br/>
     <span><b>Survival:</b> Your character has a maximum of 10 Survival, plus additional Survival equal to your Stamina (a minimum of 5 Survival if your Stamina is -5 or less). When something deals damage to your character, subtract the amount of damage from your current Survival. When something heals your character, add the amount of healing to your current Survival up to your max Survival.</span><br/>
     <br/>
-    <span><b>Death:</b> If for any reason you drop below 1 Life to zero or a negative number which is smaller in value than half your max Life, set your Life to 0 and you become unconscious and are Dying. For every turn (or minute out of combat) you are Dying, subtract 1 Life (going into negative numbers). If your Life is ever a negative number equal in value to half your max Life, you are Dead. Your character cannot be played while Dead and you must create a new character to continue playing, ask your GM what G to create your next character at and how this new character can join the story. </span><br/>
+    <span><b>Death:</b> If for any reason you drop below 1 Life to zero or a negative number which is smaller in absolute value than your max Life, set your Life to 0 and you become unconscious and are Dying. For every turn (or minute out of combat) you are Dying, subtract 1 Life (going into negative numbers). If your Life is ever a negative number equal in absolute value to your max Life, you are Dead. Your character cannot be played while Dead and you must create a new character to continue playing, ask your GM what G to create your next character at and how this new character can join the story.</span><br/>
     <hr/>
     
     <h3>Attributes:</h3>
-    <span>An Attribute is a measurement of your natural ability at a base level. No Attribute may be increased above 5 + G</span>
+    <span>An Attribute is a measurement of your natural ability at a base level. No Attribute may be increased above 3 + G</span>
     <ul>
       <li><span>Strength (Raw power, anaerobic activity)</span><br/></li>
       <li><span>Agility (Physical nimbleness and flexibility)</span><br/></li>
@@ -24,11 +24,11 @@ export default {
     <span><b>Strength Benefits:</b> Every point of Strength increases your Carry Weight by 50lb. Add your Strength value to your melee attack damage.</span><br/>
     <span><b>Agility Benefits:</b> Agility is used to avoid some area of effect attacks. Add your Agility value to your Avoidance stat.</span><br/>
     <span><b>Will Benefits:</b> Will is used to resist some non-physical effects or empower some Gifts.</span><br/>
-    <span><b>Stamina Benefits:</b> Every point of Stamina increases your Life by 1, your Survival by 1, and the distance you can travel per day by 5 miles.</span><br/>
+    <span><b>Stamina Benefits:</b> Every point of Stamina increases your Survival by 1 and the distance you can travel per day by 5 miles. Every even number of Stamina increases your Life by 1.</span><br/>
     <br/>
     <span><b>Stat Checks:</b> Some Powers, Techniques, and Attacks will contest an Attribute check. These checks can Hard Succeed or Hard Fail like a <Link to="skills">Skill</Link> check but do not give <Link to="leveling">XP</Link>.</span><br/>
     <br/>
-    <span><b>Increasing Attributes:</b> Your Attributes are determined by your Race's starting Attributes, and are increased from Attribute Points (AP) you have at character creation. When <Link to="leveling">Leveling Up</Link> at every even numbered G Level, you may increase 1 Attribute by 1, but you cannot increase the Attribute beyond [G + 3].</span><br/>
+    <span><b>Increasing Attributes:</b> Your Attributes are determined by your Race's starting Attributes, and are increased from Attribute Points (AP) you have at character creation. When <Link to="leveling">Leveling Up</Link> at every even numbered G Level, you may increase 1 Attribute by 1, but you cannot increase the Attribute beyond 3 + G.</span><br/>
     <hr/>
     <h3>Secondary Stats:</h3>
     <span><b>Shielding:</b> You start with 0 Shielding. You may gain Shielding from various effects in the game. When you have at least 1 Shielding, Attacks which reduce your Survival will first reduce your Shielding instead, if the Attack deals damage greater than your Shielding, the remaining damage after your Shielding is reduced to 0 is subtracted from your Survival. You lose any Shielding after a full sleep. </span><br/>
